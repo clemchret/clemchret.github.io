d1fe1e8f8fa06ce422d3051e30052a84f4db5d42
clemchret <clement.chret@gmail.com>
2023-08-28 02:28:04 +0200

    dev menu mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 AOS.init();
+
+const menuBurger = document.querySelector('.menu-burger');
+const menuItems = document.querySelector('.menu-items');
+
+menuBurger.addEventListener('click', function() {
+  menuBurger.classList.toggle('active');
+  menuItems.classList.toggle('active');
+});
+
+menuItems.addEventListener('click', function() {
+    menuBurger.classList.toggle('active');
+    menuItems.classList.toggle('active');
+  });
