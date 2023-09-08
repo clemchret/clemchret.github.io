90de09951acf63771c2c0c69d55f332692eaa5ff
clemchret <clement.chret@gmail.com>
2023-09-08 16:43:24 +0200

    tweaks et back to top btn

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,23 @@ menuBurger.addEventListener('click', function() {
 menuItems.addEventListener('click', function() {
     document.body.classList.remove('menu-open');
   });
+
+//Back to top btn
+const backToTopButton = document.getElementById("backToTopBtn");
+
+// Fonction pour afficher le bouton lorsque l'utilisateur fait défiler la page
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTopButton.classList.remove("hidden");
+    } else {
+        backToTopButton.classList.add("hidden");
+    }
+});
+
+// Fonction pour remonter en haut de la page lorsque le bouton est cliqué
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Animation de défilement fluide
+    });
+});
